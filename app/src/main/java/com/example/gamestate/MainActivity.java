b73ac28561f9754652fab18b9ebd4a923c29de54
Ethan <ethan.e.nguyen@outlook.com>
2023-03-14 10:20:04 -0700

    EditText incomplete

--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -5,20 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    Button run = (Button)findViewById(R.id.button);
-    run.setOnClickListener(new View.OnClickListener()
-    {
-        public void onClick(View v)
-        {
 
-        }
-    });
+    }
+
+
 }
