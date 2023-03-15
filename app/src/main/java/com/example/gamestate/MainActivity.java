ab5eb39ffeca748666ca085529294ec7d1112436
admin <kayv1273@yahoo.com>
2023-03-15 10:52:15 -0700

    Implemented part of OnClick

--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -6,24 +6,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView textView;
     private Button runTest;
+    private GameState firstInstance;
+    private GameState secondInstance;
+    private GameState thirdInstance;
+    private GameState fourthInstance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textView = findViewById(R.id.editText);
         runTest = findViewById(R.id.runTest);
         runTest.setOnClickListener(this::onClick);
+
     }
 
     public void onClick(View view) {
         if (view.getId() == runTest.getId()) {
             textView.setText("");
         }
+        firstInstance = new GameState();
+        secondInstance = new GameState(firstInstance);
+        // Call each method in the game state at least once
+        firstInstance.fillButtons();
+        //firstInstance.showPossibleMoves()
+        textView.append("Board is made."); // will fix later
+        // Needs more methods from Piece Class
+
+        thirdInstance = new GameState();
+        fourthInstance = new GameState(thirdInstance);
+
+        textView.append(secondInstance.toString());
+        textView.append(fourthInstance.toString());
     }
 }
