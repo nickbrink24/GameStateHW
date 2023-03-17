3ccceefa5c5328df51452022d1ee5d8a0d351a03
admin <kayv1273@yahoo.com>
2023-03-17 02:28:15 -0700

    Added in comments in MainActivity

--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -8,14 +8,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView textView;
-    private Button runTest;
-    private int clicks;
+    private TextView textView; // text displayed in xml
+    private Button runTest; // button runs test
+    private int clicks; // clicks made on button
     private GameState firstInstance;
     private GameState secondInstance;
     private GameState thirdInstance;
     private GameState fourthInstance;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +30,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClick(View view) {
-        // Empties
+        // Empties text
         if (view.getId() == runTest.getId()) {
             textView.setText("");
         }
-
+        // Initial board
         if (clicks == 0) {
             textView.append("Initial Board:\n" + firstInstance);
+            textView.append("Board is made.\n");
         }
-        textView.append("Board is made.\n");
         // First player will move piece in first instance
         int who = firstInstance.getTurn(); // who is whoever's turn it is
         String player = "";
@@ -48,9 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             player = "Player";
         }
+        // Change who's turn it is
         textView.append(player + "'s Turn\n");
         textView.append(player + " will move checker piece.\n");
-        // Capture a piece
+        // Capture a piece if it is valid
         if(canMove()){
             if(canCapture()){
                 textView.append(player + " has captured a piece!\n");
@@ -58,8 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             movePiece();
         }
-        clicks++;
-        firstInstance.setTurn(who);
+        clicks++; // keeps track of turn
+        firstInstance.setTurn(who); // change turn
+        // Displays updated board
         textView.append(String.valueOf(firstInstance) + "\n\n");
         if (clicks == 3) {
             textView.append("Click again.\n");
@@ -70,25 +71,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             textView.append("" + fourthInstance);
             textView.append("\n");
         }
-        //textView.append(secondInstance.toString());
-        //textView.append(fourthInstance.toString());
     }
 
+    /**
+     * Checkes if move is valid
+     * @return true if the piece can move to desired spot
+     */
     public boolean canMove() {
         boolean move = false;
         if (clicks == 0) {
+            // Red Moves
             move = firstInstance.checkPiece(firstInstance.getTurn(),firstInstance.getPieces(4,6))
             && firstInstance.checkMove(firstInstance.getTurn(),firstInstance.getPieces(4,6), firstInstance.getPieces(4,4));
         } else if (clicks == 1) {
+            // Black Moves
             move = firstInstance.checkPiece(firstInstance.getTurn(),firstInstance.getPieces(3,1))
             && firstInstance.checkMove(firstInstance.getTurn(),firstInstance.getPieces(3,1), firstInstance.getPieces(3,3));
         } else if (clicks == 2) {
+            // Red Captures Black
             move = firstInstance.checkPiece(firstInstance.getTurn(),firstInstance.getPieces(4,4))
             && firstInstance.checkMove(firstInstance.getTurn(),firstInstance.getPieces(4,4), firstInstance.getPieces(4,3));
         }
         return move;
     }
 
+    /**
+     * Moves actual piece
+     */
     public void movePiece() {
         if (clicks == 0) {
             firstInstance.setPieces(4, 4, firstInstance.getPieces(4, 6));
@@ -102,6 +111,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /**
+     * Adds captured piece to captured arraylist
+     */
     public void makeCapture() {
         int currPlayer = firstInstance.getTurn();
         if (currPlayer == 0) {
@@ -109,6 +121,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /**
+     * Checks if piece can capture at desired location
+     * @return True if they can capture and move to location
+     */
     public boolean canCapture() {
         boolean capture = false;
         if (clicks == 0) {
